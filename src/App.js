61421950dc401414544154de4ad41b43dd9a865f
Andre Soares <andreiarley@gmail.com>
2023-05-05 08:09:55 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.svg';
+import './styles/App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <HelloWorld />
     </div>
   );
 }
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,7 @@
+export default function HelloWord() {
+    return (
+        <>
+        <p>Hello World!</p>
+        </>
+    )
+}
